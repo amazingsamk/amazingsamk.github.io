22685440bbb84cf280d3f945e96ade53d736ca2f
Aashish Kapur <me@aashishkapur.com>
2015-04-13 00:32:44 -0700

    other form changes

--- FutureEngineersCamp/assets/js/form.js
+++ FutureEngineersCamp/assets/js/form.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-
+	$(".session").change(function(e){
+		var id = e.target.id;
+		// get id, class of parent function, and update array of specific id 
+		console.log("id: " + id + "       parent: " + $("#" + id).parents("div").attr('class'));
+		$("#" + id).parents("div.form-group").addClass("has-error")
+	});
 });
