c0f02f6ce620daa7678a08f991b2fe069467a5ce
Aashish Kapur <aashish97+github@gmail.com>
2015-04-14 07:16:32 -0700

    changes to form, radio selections work

--- FutureEngineersCamp/assets/js/form.js
+++ FutureEngineersCamp/assets/js/form.js
@@ -1,12 +1,36 @@
-var radio
+var radioStore = {"session":"", "gender":""};
 $(document).ready(function(){
-	$(".session").change(function(e){
+	$(".radio-group").change(function(e){
 		var id = e.target.id;
+		var targetClass = $("#" + id).parents("div").attr('class').substring(9);
+		console.log(targetClass);
+		console.log("id: " + id);
 		// get id, class of parent function, and update array of specific id 
-		console.log("id: " + id + "       parent: " + $("#" + id).parents("div").attr('class'));
+		// console.log("id: " + id + "       parent: " + $("#" + id).parents("div").attr('class'));
+		// console.log(radioStore[0][1]);
+		radioStore[targetClass] = id;
 			// $("#" + id).parents("div").attr('class') gives classes of div, 
 			// so need to parse to get specific radio group, and then put it into an array
-		$("#" + id).parents("div.form-group").addClass("has-error");
+		// $("#" + id).parents("div.form-group").addClass("has-error");
 			// makes form-group have error
 	});
 });
+
+
+function print()
+{
+	for(var i in radioStore)
+	{
+		console.log(i + ": " + radioStore[i]);
+	}
+	// return console.log(radioStore['session']);
+}
+
+
+
+
+/* var associative_array = new Array();
+associative_array["one"] = "Lorem";
+associative_array["two"] = "Ipsum";
+associative_array["three"] = "dolor";
+for (i in associative_array) { alert(i) };*/
