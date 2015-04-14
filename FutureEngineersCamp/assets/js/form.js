df03420be7c8c7faef36e67f312b7add7494bbb6
Aashish Kapur <me@aashishkapur.com>
2015-04-13 23:42:25 -0700

    minor changes

--- FutureEngineersCamp/assets/js/form.js
+++ FutureEngineersCamp/assets/js/form.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
 		var id = e.target.id;
 		// get id, class of parent function, and update array of specific id 
 		console.log("id: " + id + "       parent: " + $("#" + id).parents("div").attr('class'));
-		$("#" + id).parents("div.form-group").addClass("has-error")
+			// $("#" + id).parents("div").attr('class') gives classes of div, 
+			// so need to parse to get specific radio group, and then put it into an array
+		$("#" + id).parents("div.form-group").addClass("has-error");
+			// makes form-group have error
 	});
 });
